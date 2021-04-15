294a30c98ec2907b31af2a2b43a36e77b4d58336
Dhruv <dhruvjobanputra8@gmail.com>
2021-04-15 19:26:21 +0800

    added folders and error404.html

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello"
+    return "Hello World"
+
+@app.errorhandler(404)
+@app.route("/404")
+def error404(e):
+  return render_template("error404.html")
 
 if __name__ == "__main__":
     app.run()
