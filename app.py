02cdce540c854e01f6118cd2543ff1ce91e0fe7e
Dhruv <dhruvjobanputra8@gmail.com>
2021-04-16 14:29:53 +0800

    added form, minor changes

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Python Perfect")
 
 @app.route("/login")
 def login():
-  return render_template("login.html")
+  return render_template("login.html", title="Login")
 
 @app.errorhandler(404)
 @app.route("/404")
 def error404(e):
-  return render_template("error404.html")
+  return render_template("error404.html", title="Error 404")
 
 if __name__ == "__main__":
     app.run()
