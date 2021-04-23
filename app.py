63ce73fc2f9d3508c5fca4fc4eb7273930bed013
Dhruv <dhruvjobanputra8@gmail.com>
2021-04-23 18:50:24 +0800

    getting current user from login

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", title="Python Perfect")
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-  return render_template("login.html", title="Login")
+  if request.method == "POST":
+    current_user = request.form["username"] 
+    return redirect(url_for("dashboard", user=current_user))
+  else:
+    return render_template("login.html", title="Login")
 
 @app.route("/signup")
 def signup():
   return render_template("signup.html", title="Sign Up")
+
+@app.route("/dashboard/<user>")
+def dashboard(user):
+  return render_template("dashboard.html", title="Dashboard", user=user)
 
 @app.errorhandler(404)
 @app.route("/404")
@@ -18,4 +26,4 @@ def error404(e):
   return render_template("error404.html", title="Error 404")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
