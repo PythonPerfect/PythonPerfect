006f2c35cc8ecf93d0b73b4e7708b15d695f9bdf
Dhruv <dhruvjobanputra8@gmail.com>
2021-05-07 14:57:51 +0800

    hotfix/added database

--- PythonPerfect.py
+++ PythonPerfect.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User
+from app.models import User, Course
 
 @app.shell_context_processor
 def add_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Course': Course}
