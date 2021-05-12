264177aba27dd042105fa1de0472146eff595a41
papasheng <wassuprt@gmail.com>
2021-05-12 09:03:53 +0800

    Revert "Database Complete. Testing Required."
    This reverts commit 258f1187647226bd4e12d17048774a042a5179d4.

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db, render_as_batch=True)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'You must be loged in to access this!'
