872a4a40d98d7942e15d8dfe32527245e3eff855
papasheng <wassuprt@gmail.com>
2021-05-12 09:08:24 +0800

    Revert "Revert "Database Complete. Testing Required.""
    This reverts commit 264177aba27dd042105fa1de0472146eff595a41.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import backref
+from sqlalchemy.sql.expression import false, true
 from app import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -8,6 +10,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     admin = db.Column(db.Boolean, default=False)
+
+    #Backref
+    question_responses = db.relationship('Question_Response', backref='user', lazy='dynamic')
+    contents_viewed = db.relationship('Content_Viewed', backref='user', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -26,14 +32,76 @@ class Course(db.Model):
     id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
     title = db.Column(db.String(50), index=True, unique=True)
 
+    #Backref
+    contents = db.relationship('Content', backref='course', lazy='dynamic')
+    quizzes = db.relationship('Quiz', backref='course', lazy='dynamic')
+
     def __repr__(self):
         return '<Course {}>'.format(self.title)
 
 class Content(db.Model):
     id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
-    course_id = db.Column(db.Integer)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     title = db.Column(db.String(50))
     text = db.Column(db.Text)
 
+    #Foreign Key
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+    #Backref
+    contents_viewed = db.relationship('Content_Viewed', backref='content', lazy='dynamic')
+
     def __repr__(self):
         return '<Content {}>'.format(self.title)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
+    title = db.Column(db.String(50))
+
+    #Foreign Key
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+    #Backref
+    questions = db.relationship('Question', backref='quiz', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Quiz {}>'.format(self.id)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
+    question = db.Column(db.String(256))
+    answer = db.Column(db.String(32))
+
+    #Foreign Key
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+
+    #Backref
+    question_response = db.relationship('Question_Response', backref='question', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Question {}>'.format(self.body)
+
+class Question_Response(db.Model):
+    id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
+    response = db.Column(db.Integer)
+    
+    #Foreign Key
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Question_Response {}>'.format(self.response)
+
+    def check_correct(self):
+        return self.response == self.question.answer
+
+class Content_Viewed(db.Model):
+    id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
+    viewed = db.Column(db.Boolean, default=False)
+
+    #Foreign Key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content_id = db.Column(db.Integer, db.ForeignKey('content.id'))
+
+    def __repr__(self):
+        return '<Content_Viewed {}>'.format(self.viewed)
