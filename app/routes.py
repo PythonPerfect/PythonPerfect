cf61d09e1a05d9af7eb26d08d7768c94832d6ab3
Dhruv <dhruvjobanputra8@gmail.com>
2021-05-17 20:02:26 +0800

    content view bug, remove delete modals, profile completion

--- app/routes.py
+++ app/routes.py
@@ -170,7 +170,7 @@ def edited_content(content_id):
 @login_required
 def view_content(content_id):
   content = get_content_by_id(content_id)
-  add_new_content_viewed(viewed=True, user=current_user, content=content)
+  add_new_content_viewed(user=current_user, content=content)
 
   return render_template("view-content.html", content=content, title=content.title)
 # -----------------------------------------------------------------------------
